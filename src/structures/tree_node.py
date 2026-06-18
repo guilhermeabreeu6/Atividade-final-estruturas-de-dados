802a162d83daa6250a504e92df32835dea2c66d6
guilhermeabreeu6 <guilhermeabreeu6@gmail.com>
2026-06-18 10:07:48 -0300

    cria estrutura de no da arvore

--- src/structures/tree_node.py
+++ src/structures/tree_node.py
@@ -1,8 +1,12 @@
-
- # Classe que representa um nó em uma árvore binária.
- 
 class TreeNode:
     def __init__(self, value):
+        # Guarda o valor principal do nó.
         self.value = value
+
+        # Referência para o filho da esquerda.
+        # Na árvore binária de busca, aqui ficam os valores menores.
         self.left = None
+
+        # Referência para o filho da direita.
+        # Na árvore binária de busca, aqui ficam os valores maiores.
         self.right = None
